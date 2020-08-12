d6376e6407fce178ef6f0278de4b1c2098bd7b16
BorjaEst <boressan@outlook.com>
2020-08-12 13:43:53 +0200

    Add py package template with hello world function

---
+++ cicd/module_template.py
@@ -0,0 +1,8 @@
+"""Python module template."""
+
+
+def hello_world(*arg, **kvarg):
+    print("Hello World")
+    if arg or kvarg:
+        print(f"  - Arguments as inputs: {arg}")
+        print(f"  - Key-Value arguments: {kvarg}")
