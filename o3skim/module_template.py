9a34b212991cbfb6bda52199aaa809c8e2fb6693
BorjaEst <boressan@outlook.com>
2020-08-20 15:51:30 +0200

    Del module_template

--- o3skim/module_template.py
+++
@@ -1,9 +0,0 @@
-"""Python module template."""
-
-
-def hello_world(*arg, **kvarg):
-    print("Hello World")
-    if arg or kvarg:
-        print(f"  - Arguments as Inputs: {arg}")
-        print(f"  - Arguments as KeyVal: {kvarg}")
-    return True
