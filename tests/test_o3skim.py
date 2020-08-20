b2d3418b1e9f31394b14a4d51be566003ae50df9
BorjaEst <boressan@outlook.com>
2020-08-20 15:40:42 +0200

    Edit unittests to check standard load of sources

--- tests/test_o3skim.py
+++ tests/test_o3skim.py
@@ -3,8 +3,10 @@
 
 import os
 import unittest
+import xarray as xr
+import glob
 
-from o3skim import sources
+from o3skim import sources, utils
 # from pyfakefs.fake_filesystem_unittest import TestCase
 from . import mockup_data
 
@@ -17,19 +19,60 @@ class TestO3SKIM_sources(unittest.TestCase):
         self.config = sources.load("tests/test_sources.yaml")
         self.assertTrue(type(self.config) is dict)
 
+        self.create_mock_datasets()
+        self.backup_datasets()
+        self.assert_with_backup()
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def create_mock_datasets(self):
+        """Creates mock data files according to the loaded configuration"""
         for source, collection in self.config.items():
             for model, variables in collection.items():
                 for v, vinfo in variables.items():
                     path = "data/" + vinfo["dir"]
                     os.makedirs(path, exist_ok=True)
-                    for n in range(3):
-                        file_path = path + "/source_file_" + str(n) + ".nc"
-                        mockup_data.netcdf(file_path, **vinfo)
+                    mockup_data.netcdf(path, **vinfo)
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+    def backup_datasets(self):
+        """Loads the mock datasets into an internal variable"""
+        self.ds_backup = {}
+        for source, collection in self.config.items():
+            self.ds_backup[source] = {}
+            for model, variables in collection.items():
+                self.ds_backup[source][model] = {}
+                for v, vinfo in variables.items():
+                    paths = "data/" + vinfo["dir"] + "/*.nc"
+                    with xr.open_mfdataset(paths) as ds:
+                        self.ds_backup[source][model][v] = ds
 
-    def test_000_something(self):
-        """Test something."""
-        self.assertTrue(True)
-        self.assertFalse(False)
+    def assert_with_backup(self):
+        """Asserts the dataset in the backup is equal to the config load"""
+        for source, collection in self.config.items():
+            for model, variables in collection.items():
+                for v, vinfo in variables.items():
+                    paths = "data/" + vinfo["dir"] + "/*.nc"
+                    with xr.open_mfdataset(paths) as ds:
+                        xr.testing.assert_identical(
+                            self.ds_backup[source][model][v], ds)
+
+    def test_000_SourcesFromConfig(self):
+        """Creates the different sources from the configuration file"""
+        with utils.cd("data"):
+            ds = {source: sources.Source(collection) for
+                  source, collection in self.config.items()}
+
+        # CCMI-1 tco3_zm asserts
+        self.assertTrue('time' in ds['CCMI-1'].model['IPSL'].tco3_zm.coords)
+        self.assertTrue('lon' in ds['CCMI-1'].model['IPSL'].tco3_zm.coords)
+        self.assertTrue('lat' in ds['CCMI-1'].model['IPSL'].tco3_zm.coords)
+
+        # CCMI-1 vrm_zm asserts
+        self.assertTrue('time' in ds['CCMI-1'].model['IPSL'].vrm_zm.coords)
+        self.assertTrue('plev' in ds['CCMI-1'].model['IPSL'].vrm_zm.coords)
+        self.assertTrue('lon' in ds['CCMI-1'].model['IPSL'].vrm_zm.coords)
+        self.assertTrue('lat' in ds['CCMI-1'].model['IPSL'].vrm_zm.coords)
+
+        # Checks the original data has not been modified
+        self.assert_with_backup()
