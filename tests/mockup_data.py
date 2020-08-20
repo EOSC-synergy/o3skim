4e3d4af4f206ee13f60655fd9882784f4bf13e67
BorjaEst <boressan@outlook.com>
2020-08-20 10:17:42 +0200

    Added function descriptions to mockup_data.py

--- tests/mockup_data.py
+++ tests/mockup_data.py
@@ -13,17 +13,20 @@ size = {'time': 20, 'plev':   10, 'lat':  18, 'lon':   36}
 
 
 def data_vars(coordinades):
+    """Creates a mock n-array with coordinade values"""
     alias = tuple(coordinades.values())
     dim = tuple(size[c] for c in coordinades)
     return alias, np.ones(dim),
 
 
 def map_coord(coordinades):
+    """Creates a mock array by coordinade"""
     return {alias: [x for x in range(start[c], end[c], step[c])]
             for c, alias in coordinades.items()}
 
 
 def netcdf(fname, name, coordinades, **kwarg):
+    """Creates or appends data to a mock netcdf file"""
     if not os.path.isfile(fname):
         create_empty_netCDF(fname)
 
@@ -35,6 +38,7 @@ def netcdf(fname, name, coordinades, **kwarg):
 
 
 def create_empty_netCDF(fname):
+    """Creates a new empty netCDF file"""
     root_grp = netCDF4.Dataset(fname, 'w', format='NETCDF4')
     root_grp.description = 'Example simulation data'
     root_grp.close()
