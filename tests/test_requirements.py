c00288fe471f97243d9b7c3a6adf9a2dd033d83c
BorjaEst <boressan@outlook.com>
2021-12-07 13:49:40 +0100

    Add fixtures for context folder

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -1,13 +1,25 @@
 """Simple test module for testing"""
-from tests.mockup import cf_19 as datamock
+import os
+
 from pytest import fixture
 
-
-@fixture()
-def dataset():
-    return datamock.toz_dataset()
+from tests.mockup import cf_19 as datamock
 
 
-def test_simple(dataset):
-    dataset.to_netcdf("delete.nc")
+@fixture(scope="session")
+def dataset_folder(tmpdir_factory):
+    tmpdir = tmpdir_factory.mktemp("data")
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(tmpdir))
+    yield
+    os.chdir(prevdir)
+
+
+@fixture(scope="module", params=["toz_cf19.nc"])
+def dataset_file(request, dataset_folder):
+    datamock.toz_dataset().to_netcdf(request.param)
+    return request.param
+
+
+def test_simple(dataset_file):
     assert True
