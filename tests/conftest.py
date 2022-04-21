457fec09d20d1be4024cb5075a7a178ffb2d6693
Borja Esteban <boressan@outlook.com>
2022-04-21 13:54:22 +0200

    replace o3mocks by specific small cf_datasets

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,48 +1,28 @@
 """Fixtures module for pytest"""
-import os
-
 import xarray as xr
-import o3mocks
 from pytest import fixture
 
 
-@fixture(scope="session", autouse=True)
-def add_pythonpath():
-    import sys
-
-    sys.path.insert(0, str(os.getcwd()))
-
-
-@fixture(scope="session", autouse=True)
-def dataset_folder(tmpdir_factory):
-    tmpdir = tmpdir_factory.mktemp("data")
-    return os.path.expanduser(tmpdir)
-
-
-@fixture(scope="session", params=["toz_cf18"])
-def netCDF_file(request, dataset_folder):
-    filename = f"{dataset_folder}/{request.param}.nc"
-    o3mocks.cf_18.toz_dataset(filename)
-    return filename
-
-
-@fixture(scope="session", params=["atmosphere_mole_content_of_ozone"])
-def variable(request):
+@fixture(
+    scope="session",
+    params=[
+        "tests/datasets/toz_std.1.8.nc",
+        "tests/datasets/no_bounds/*.nc",
+        "tests/datasets/no_cellmx/*.nc",
+        "tests/datasets/splitted/*.nc",
+    ],
+)
+def dataset_path(request):
     return request.param
 
 
 @fixture(scope="session")
-def netCDF_files(netCDF_file):
-    ds = xr.open_dataset(netCDF_file)
-    ds = ds.cf.rename({"toz": "toz_zm"})
-    dates, datasets = zip(*ds.groupby("time"))
-    paths = [f"{netCDF_file[:-3]}_{t}.nc" for t in dates]
-    xr.save_mfdataset(datasets, paths)
-    return paths
+def dataset(dataset_path):
+    kwargs = dict(data_vars='minimal', concat_dim='time', combine='nested')
+    with xr.open_mfdataset(dataset_path, **kwargs) as ds:
+        yield ds
 
 
 @fixture(scope="session")
-def dataset(netCDF_files, variable):
-    kwargs = dict(data_vars="minimal", concat_dim="time", combine="nested")
-    dataset = xr.open_mfdataset(netCDF_files, **kwargs)
-    return dataset.cf[[variable]]
+def variable():
+    return "atmosphere_mole_content_of_ozone"
