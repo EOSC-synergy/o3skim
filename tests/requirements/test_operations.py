cd70e1a391a68e8a78a01ae8555a5cd5bde6a4e5
Borja Esteban <boressan@outlook.com>
2022-04-21 10:28:29 +0200

    Assert bounds are rm with skim opertations

--- tests/requirements/test_operations.py
+++ tests/requirements/test_operations.py
@@ -1,11 +1,31 @@
 """Simple test module for testing"""
 from pytest import fixture, mark
 
-from . import (
-    LatSkimRequirements,
-    LonSkimRequirements,
-    VariableRequirements,
-)
+
+class VariableRequirements:
+    def test_standard_name(self, standard_name):
+        assert standard_name == "atmosphere_mole_content_of_ozone"
+
+    def test_standard_unit(self, units):
+        assert units == "mol m-2"
+
+
+class LatSkimRequirements:
+    def test_no_latitude(self, skimmed, dataset):
+        assert "latitude" not in skimmed.cf.coordinates
+        assert dataset.cf["latitude"].attrs["bounds"] not in skimmed
+
+    def test_cell_methods(self, cell_methods):
+        assert " latitude: mean" in cell_methods
+
+
+class LonSkimRequirements:
+    def test_no_longitude(self, skimmed, dataset):
+        assert "longitude" not in skimmed.cf.coordinates
+        assert dataset.cf["longitude"].attrs["bounds"] not in skimmed
+
+    def test_cell_methods(self, cell_methods):
+        assert " longitude: mean" in cell_methods
 
 
 @mark.parametrize("operations", [["lat_mean"]], indirect=True)
