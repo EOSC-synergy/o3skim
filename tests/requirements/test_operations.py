40bb0143fb1bd0416aeaef3d542bb30a8386fe41
Borja Esteban <boressan@outlook.com>
2022-05-05 15:58:38 +0200

    Refactor test_operations

--- tests/requirements/test_operations.py
+++ tests/requirements/test_operations.py
@@ -1,44 +1,69 @@
 """Simple test module for testing"""
-from pytest import mark
+import o3skim
+from pytest import fixture, mark
+
+VAR = "atmosphere_mole_content_of_ozone"
+
+# Module fixtures ---------------------------------------------------
+@fixture(scope="class", params=["standard", "with_methods", "no_bounds"])
+def dataset(request, dataset_gen):
+    dataset = dataset_gen()
+    if request.param == "no_methods":
+        del dataset[VAR].attrs["cell_methods"]
+    if request.param == "no_bounds":
+        dataset = dataset.drop_vars(["time_bounds"])
+        del dataset["time"].attrs["bounds"]
+        dataset = dataset.drop_vars(["longitude_bounds"])
+        del dataset["longitude"].attrs["bounds"]
+        dataset = dataset.drop_vars(["latitude_bounds"])
+        del dataset["latitude"].attrs["bounds"]
+    return dataset
 
 
+# Requirements ------------------------------------------------------
 class VariableRequirements:
-    def test_standard_name(self, standard_name):
-        assert standard_name == "atmosphere_mole_content_of_ozone"
+    @mark.parametrize("dataset", ["no_methods"], indirect=True)
+    def test_var_attrs(self, dataset, skimmed):
+        assert dataset[VAR].attrs == skimmed[VAR].attrs
 
-    def test_standard_unit(self, units):
-        assert units == "mol m-2"
+    def test_global_attrs(self, dataset, skimmed):
+        assert dataset.attrs == skimmed.attrs
 
 
 class LatSkimRequirements:
     def test_no_latitude(self, skimmed):
         assert "latitude" not in skimmed.cf.coordinates
 
-    @mark.xfail(raises=KeyError)
+    @mark.parametrize("dataset", ["standard", "no_methods"], indirect=True)
     def test_no_lat_bounds(self, skimmed, dataset):
         assert dataset.cf["latitude"].attrs["bounds"] not in skimmed
 
-    def test_cell_methods(self, cell_methods):
-        assert " latitude: mean" in cell_methods
+    @mark.parametrize("dataset", ["standard", "no_bounds"], indirect=True)
+    def test_cell_methods(self, skimmed):
+        assert "latitude: mean" in skimmed[VAR].attrs["cell_methods"]
 
 
 class LonSkimRequirements:
     def test_no_longitude(self, skimmed):
         assert "longitude" not in skimmed.cf.coordinates
 
-    @mark.xfail(raises=KeyError)
+    @mark.parametrize("dataset", ["standard", "no_methods"], indirect=True)
     def test_no_lon_bounds(self, skimmed, dataset):
         assert dataset.cf["longitude"].attrs["bounds"] not in skimmed
 
-    def test_cell_methods(self, cell_methods):
-        assert " longitude: mean" in cell_methods
+    @mark.parametrize("dataset", ["standard", "no_bounds"], indirect=True)
+    def test_cell_methods(self, skimmed):
+        assert "longitude: mean" in skimmed[VAR].attrs["cell_methods"]
 
 
-@mark.parametrize("operations", [["lat_mean"]], indirect=True)
+# Parametrization ---------------------------------------------------
 class TestLatMean(VariableRequirements, LatSkimRequirements):
-    pass
+    @fixture(scope="class", autouse=True)
+    def skimmed(self, dataset):
+        return o3skim.lat_mean(dataset)
 
 
-@mark.parametrize("operations", [["lon_mean"]], indirect=True)
 class TestLonMean(VariableRequirements, LonSkimRequirements):
-    pass
+    @fixture(scope="class", autouse=True)
+    def skimmed(self, dataset):
+        return o3skim.lon_mean(dataset)
