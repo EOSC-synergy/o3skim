58fcf0f5924082f23f322317c7305d12f63ae5b0
Borja Esteban <boressan@outlook.com>
2022-05-05 15:58:30 +0200

    Refactor test_utils

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,84 @@
 """Fixtures module for pytest"""
+import numpy as np
+import pandas as pd
+import xarray as xr
+from pytest import fixture
+
+
+@fixture(scope="class")
+def dataset_gen():
+    return lambda: xr.Dataset(
+        data_vars=dict(
+            atmosphere_mole_content_of_ozone=xr.Variable(
+                ["longitude", "latitude", "time"],
+                15 + 8 * np.random.randn(2, 2, 3),
+                attrs=dict(
+                    units="mol m-2",
+                    long_name="Total column of ozone in the atmosphere",
+                    standard_name="atmosphere_mole_content_of_ozone",
+                    cell_methods="area: max",
+                ),
+            ),
+            longitude_bounds=xr.Variable(
+                ["longitude", "bounds"],
+                [(0, 180), (180, 360)],
+            ),
+            latitude_bounds=xr.Variable(
+                ["latitude", "bounds"],
+                [(-90, 0), (0, 90)],
+            ),
+            time_bounds=xr.Variable(
+                ["time", "bounds"],
+                list(
+                    zip(
+                        pd.date_range("2014-09-06", periods=3),
+                        pd.date_range("2014-09-07", periods=3),
+                    )
+                ),
+            ),
+        ),
+        coords=dict(
+            longitude=xr.Variable(
+                ["longitude"],
+                [90, 270],
+                attrs=dict(
+                    standard_name="longitude",
+                    axis="X",
+                    units="degrees_east",
+                    long_name="Longitude of the grid center",
+                    bounds="longitude_bounds",
+                    valid_range=np.array([0.0, 360.0]),
+                ),
+            ),
+            latitude=xr.Variable(
+                ["latitude"],
+                [-45, 45],
+                attrs=dict(
+                    units="degrees_north",
+                    long_name="Latitude of the grid center",
+                    axis="Y",
+                    standard_name="latitude",
+                    bounds="latitude_bounds",
+                    valid_range=np.array([-90.0, 90.0]),
+                ),
+            ),
+            time=xr.Variable(
+                ["time"],
+                pd.date_range("2014-09-06", periods=3),
+                attrs=dict(
+                    standard_name="time",
+                    axis="T",
+                    long_name="time",
+                    bounds="time_bounds",
+                ),
+            ),
+            bounds=xr.Variable(["bounds"], [0, 1]),
+            reference_time=pd.Timestamp("2014-09-05"),
+        ),
+        attrs=dict(
+            Conventions="CF-1.5",
+            title="NetCDF CF file prov...servations",
+            references="GTO-ECV reference: L...318, 2009.",
+            comment="Product generated at...I project.",
+        ),
+    )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,80 +1,23 @@
-import numpy as np
-import pandas as pd
-import xarray as xr
 from o3skim import utils
 from pytest import fixture, mark
 
 
-# Module fixtures ---------------------------------------------------
 @fixture(scope="class")
-def dataset():
-    return xr.Dataset(
-        data_vars=dict(
-            temperature=xr.Variable(
-                ["x", "y", "time"],
-                15 + 8 * np.random.randn(2, 2, 3),
-                attrs=dict(
-                    units="K",
-                    long_name="Air temperature",
-                    standard_name="air_temperature",
-                ),
-            ),
-            precipitation=xr.Variable(
-                ["x", "y", "time"],
-                10 * np.random.rand(2, 2, 3),
-                attrs=dict(
-                    units="kg m-2",
-                    long_name="Amount means mass per unit area.",
-                    standard_name="precipitation_amount",
-                ),
-            ),
-        ),
-        coords=dict(
-            lon=xr.Variable(
-                ["x", "y"],
-                [[-99.83, -99.32], [-99.79, -99.23]],
-                attrs=dict(
-                    units="degrees_east",
-                    long_name="Longitude of the grid center",
-                    axis="X",
-                    standard_name="longitude",
-                    valid_range=np.array([0.0, 360.0]),
-                ),
-            ),
-            lat=xr.Variable(
-                ["x", "y"],
-                [[42.25, 42.21], [42.63, 42.59]],
-                attrs=dict(
-                    units="degrees_north",
-                    long_name="Latitude of the grid center",
-                    axis="Y",
-                    standard_name="latitude",
-                    valid_range=np.array([-90.0, 90.0]),
-                ),
-            ),
-            time=xr.Variable(
-                ["time"],
-                pd.date_range("2014-09-06", periods=3),
-                attrs=dict(
-                    long_name="time",
-                    axis="T",
-                    standard_name="time",
-                ),
-            ),
-            reference_time=pd.Timestamp("2014-09-05"),
-        ),
-        attrs=dict(
-            Conventions="CF-1.5",
-            title="NetCDF CF file prov...servations",
-            references="GTO-ECV reference: L...318, 2009.",
-            comment="Product generated at...I project.",
-            non_standard=True,
-        ),
-    )
+def dataset(dataset_gen):
+    dataset = dataset_gen()
+    dataset["atmosphere_mole_content_of_ozone"].attrs["non_standard"] = True
+    dataset.attrs["non_standard"] = True
+    dataset["time"].attrs["non_standard"] = True
+    dataset["latitude"].attrs["non_standard"] = True
+    dataset["longitude"].attrs["non_standard"] = True
+    return dataset
 
 
-# Requirements ------------------------------------------------------
-class ReqGlobalCF:
+class TestCleanGlobal:
+    @fixture(scope="class", autouse=True)
+    def clean_attributes(self, dataset):
+        utils.cf_clean(dataset)
+
     def test_rm_non_standard(self, dataset):
         assert "non_standard" not in dataset.attrs
 
@@ -91,7 +34,12 @@ class ReqGlobalCF:
         assert "comment" in dataset.attrs
 
 
-class ReqCoordsCF:
+@mark.parametrize("coord", ["time", "longitude", "latitude"])
+class TestCleanCoordinates:
+    @fixture(scope="class", autouse=True)
+    def clean_attributes(self, dataset):
+        utils.cf_clean(dataset)
+
     def test_rm_non_standard(self, dataset, coord):
         assert "non_standard" not in dataset[coord].attrs
 
@@ -105,7 +53,12 @@ class ReqCoordsCF:
         assert "axis" in dataset[coord].attrs
 
 
-class ReqVarsCF:
+@mark.parametrize("var", ["atmosphere_mole_content_of_ozone"])
+class TestCleanVariables:
+    @fixture(scope="class", autouse=True)
+    def clean_attributes(self, dataset):
+        utils.cf_clean(dataset)
+
     def test_rm_non_standard(self, dataset, var):
         assert "non_standard" not in dataset[var].attrs
 
@@ -117,24 +70,3 @@ class ReqVarsCF:
 
     def test_keep_units(self, dataset, var):
         assert "units" in dataset[var].attrs
-
-
-# Parametrization ---------------------------------------------------
-class TestCleanGlobal(ReqGlobalCF):
-    @fixture(scope="class", autouse=True)
-    def clean_attributes(self, dataset):
-        utils.cf_clean(dataset)
-
-
-@mark.parametrize("coord", ["time", "lon", "lat"])
-class TestCleanCoordinates(ReqCoordsCF):
-    @fixture(scope="class", autouse=True)
-    def clean_attributes(self, dataset):
-        utils.cf_clean(dataset)
-
-
-@mark.parametrize("var", ["temperature", "precipitation"])
-class TestCleanVariables(ReqVarsCF):
-    @fixture(scope="class", autouse=True)
-    def clean_attributes(self, dataset):
-        utils.cf_clean(dataset)
