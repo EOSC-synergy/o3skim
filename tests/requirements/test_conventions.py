b5b64f16bc934a803ac29c2216f886212f850c8e
Borja Esteban <boressan@outlook.com>
2022-05-05 14:41:02 +0200

    Rename test_loads by test_conventions

--- tests/requirements/test_conventions.py
+++ tests/requirements/test_conventions.py
@@ -1,81 +1,69 @@
 """Simple test module for testing"""
+from os import listdir
 
-from subprocess import run
-
-import o3skim.scripts.skim_tco3 as skim_tco3
-import xarray as xr
+from o3skim import loads
+from o3skim.attributes import global_attributes
 from pytest import fixture, mark
 
 
-@fixture(scope="module")
-def paths(dataset_path):
-    return dataset_path
-
-
-@fixture(scope="module")
-def operations(lon_mean, lat_mean):
-    operations = []
-    if lon_mean:
-        operations.append("lon_mean")
-    if lat_mean:
-        operations.append("lat_mean")
-    return operations if operations else None
-
-
-@fixture(scope="module", params=[False, True])
-def lon_mean(request):
+# Module fixtures ---------------------------------------------------
+@fixture(scope="class", params=[])
+def source(request):
     return request.param
 
 
-@fixture(scope="module", params=[False, True])
-def lat_mean(request):
-    return request.param
+@fixture(scope="class")
+def paths(source, model):
+    return f"tests/datasets/{source}/{model}/*.nc"
 
 
-@fixture(scope="module", params=["atmosphere_mole_content_of_ozone"])
-def variable_name(request):
-    return request.param
+# Requirements ------------------------------------------------------
+class AttrRequirements:
+    def test_variable(self, dataset):
+        assert "toz" in set(dataset.variables)
+        if dataset.cf.bounds:  # Bounds are variables
+            assert len(dataset.cf.data_vars) == 4
+        else:
+            assert len(dataset.cf.data_vars) == 1
+
+    def test_coordinates(self, dataset):
+        assert set(dataset.coords) == {"time", "lat", "lon"}
+        assert dataset["time"].standard_name == "time"
+        assert dataset["lat"].standard_name == "latitude"
+        assert dataset["lon"].standard_name == "longitude"
+
+    def test_bounds(self, dataset):
+        if dataset.cf.bounds:  # Not required
+            assert dataset.cf.bounds["T"] == ["time_bnds"]
+            assert dataset.cf.bounds["X"] == ["lon_bnds"]
+            assert dataset.cf.bounds["Y"] == ["lat_bnds"]
+
+    @mark.parametrize("attrs", [global_attributes().index])
+    def test_attributes(self, dataset, attrs):
+        assert all(k in attrs for k in dataset.attrs)
+        assert "Conventions" in dataset.attrs
+        assert "institution" in dataset.attrs
+        assert "source" in dataset.attrs
 
 
-@fixture(scope="module")
-def options(verbosity, output, original_attributes):
-    return dict(
-        verbosity=verbosity,
-        output=output,
-        original_attributes=original_attributes,
-    )
+# Parametrization ---------------------------------------------------
+@mark.parametrize("source", ["CCMI-1"], indirect=True)
+class TestCCMI(AttrRequirements):
+    @fixture(scope="class", params=listdir("tests/datasets/CCMI-1"))
+    def model(self, request):
+        return request.param
+
+    @fixture(scope="class")
+    def dataset(self, paths):
+        return loads.ccmi(paths)
 
 
-@fixture(scope="module", params=["DEBUG", "ERROR"])
-def verbosity(request):
-    return request.param
+@mark.parametrize("source", ["ESACCI"], indirect=True)
+class TestESACCI(AttrRequirements):
+    @fixture(scope="class", params=listdir("tests/datasets/ESACCI"))
+    def model(self, request):
+        return request.param
 
-
-@fixture(scope="module", params=["toz-skimmed.nc"])
-def output(request, tmpdir_factory):
-    tmpdir = tmpdir_factory.mktemp("data")
-    return str(tmpdir.join(request.param))
-
-
-@fixture(scope="module", params=[False])
-def original_attributes(request):
-    return request.param
-
-
-@fixture(scope="module", autouse=True)
-def skim(paths, operations, variable_name, options):
-    skim_tco3.process(paths, operations, variable_name, **options)
-
-
-def test_originals_not_edited(dataset, paths):
-    kwargs = dict(data_vars="minimal", concat_dim="time", combine="nested")
-    assert dataset == xr.open_mfdataset(paths, **kwargs)
-
-
-# ERROR: (7.1): boundary variable with non-numeric data type
-# ERROR: (7.1): Boundary var time_bnds has inconsistent units to time
-# ERROR: (7.1): Incorrect number of dimensions for boundary variable: lat_bnds
-@mark.skip(reason="xarray errors when saving CF files")
-def test_cf_conventions(output):
-    cmd = f"cfchecks -v auto {output}"
-    assert 0 == run(cmd, capture_output=True, shell=True).returncode
+    @fixture(scope="class")
+    def dataset(self, paths):
+        return loads.esacci(paths)
